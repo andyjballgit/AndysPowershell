6cdfd5fa662101084fe5a27067d60b2c738c7c23
Andy Ball <Andy@Fake.com>
2017-02-24 16:02:33 +0000

    add StartDocker.ps1

---
+++ PowerShellModuleProject1/NotProductionQuality/StartDocker.ps1
@@ -0,0 +1,17 @@
+﻿
+$ContainerPrefix = "SQLRocks"
+$PortNumberStart = 1501
+$PortNumber = $PortNumberStart
+$Instances = 2 
+$PackageName = 
+
+For($Counter = 1 ; $Counter -le $Instances; $Counter++)
+    {
+        $ContainerName = $ContainerPrefix + $PortNumber
+        Write-Host "Starting Container = $ContainerName ($Counter of $Instances) on Port = $PortNumber"
+        $MyArgs = "run --name $ContainerName -d -p $PortNumber" + ":" + $PortNumber + " -e sa_password= -e ACCEPT_EULA=Y microsoft/mssql-server-windows-developer"
+        
+        Start-Process -FilePath docker.exe -ArgumentList $MyArgs -Wait
+
+        $PortNumber++
+    }
