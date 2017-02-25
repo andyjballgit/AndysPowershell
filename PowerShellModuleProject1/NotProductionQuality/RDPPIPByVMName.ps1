8ceb660c9e7a162736c397c188c74fbab4d06412
Andy Ball <Andy@Fake.com>
2017-02-25 07:58:55 +0000

    Add RDPPIPByVMName.ps1

---
+++ PowerShellModuleProject1/NotProductionQuality/RDPPIPByVMName.ps1
@@ -0,0 +1,15 @@
+﻿$VMName = "SomeServer"
+
+$VM = Get-AzureRMVM | Where Name -eq $VMName 
+
+$Resource  = Get-AzureRMResource -ResourceId $VM.NetworkProfile.NetworkInterfaces.iD 
+$PIP = Get-AzureRMResource -ResourceId ($Resource.Properties.ipConfigurations[0].properties.publicIPAddress.id) 
+$PIPAddress = $Pip.Properties.ipAddress
+
+$Params =  "/v:$PIPAddress"
+Start-Process -FilePath "MSTSC.exe" -ArgumentList $Params 
+
+
+
+
+
